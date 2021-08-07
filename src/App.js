f75be6f897bf043eb2b5adefab3aad5ae77d4fe4
Winswins22 <73000223+Winswins22@users.noreply.github.com>
2021-08-07 17:08:12 -0400

    results

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Home } from './components'
 import GlobalStyle from './GlobalStyles'
+import Results from './components/Results/Results'
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Home />
+      <Results />
 
     </>
   );
